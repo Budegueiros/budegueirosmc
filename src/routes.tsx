76207cbeeb6b15b2985817db623ba602626e5257
Weslei Santos <wosantos2@hotmail.com>
2025-01-07 22:37:54 -0300

    instalção e config tailwindcss

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,11 @@ export const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/home' element={<p>home</p>} />
+                <Route path='/home' element={
+                    <h1 className="text-3xl font-bold underline">
+                        Hello world!
+                    </h1>
+                } />
                 <Route path='/home2' element={<p>home 2</p>} />
 
                 <Route path='*' element={<Navigate to='/home' />} />
